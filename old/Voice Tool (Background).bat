b531795e534778eab93c36f76bf2792030efbc46
Nolyo <yohannjaffres88@gmail.com>
2026-04-10 19:10:05 +0200

    Remove old project files

--- old/Voice Tool (Background).bat
+++
@@ -1,3 +0,0 @@
-@echo off
-rem Launch from the directory of this .bat (Windows path)
-powershell -NoProfile -WindowStyle Hidden -Command "Set-Location '%~dp0'; python main.py"
--- old/Voice Tool (Console).bat
+++
@@ -1,4 +0,0 @@
-@echo off
-rem Launch from the directory of this .bat (Windows path)
-powershell -NoProfile -Command "Set-Location '%~dp0'; python main.py --console"
-pause
